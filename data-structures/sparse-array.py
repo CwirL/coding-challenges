ca823110d376a9cc1e9e99057c33b58d4b2f72e9
cwirl <cwilmer@uninorte.edu.co>
2020-02-08 19:15:33 -0500

    chore: sparse array

---
+++ data-structures/sparse-array.py
@@ -0,0 +1,30 @@
+import re
+
+
+def matchingStrings(strings, queries):
+    sols = []
+    for query in queries:
+        rep = 0
+        for word in strings:
+            if word == query:
+                rep += 1
+        sols.append(rep)
+    return sols
+
+
+file = open('./data', 'r')
+strings_count = int(file.readline())
+strings = []
+for _ in range(strings_count):
+    strings.append(file.readline().rstrip())
+
+queries_count = int(file.readline())
+queries = []
+for _ in range(queries_count):
+    queries.append(file.readline().rstrip())
+print(strings)
+print(queries)
+
+res = matchingStrings(strings, queries)
+for sol in res:
+    print(sol)
