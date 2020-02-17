84be347cd1c9e8e899f3a5a68774f44fe532e38e
cwirl <cwilmer@uninorte.edu.co>
2020-02-17 12:34:24 -0500

    chore: Insert node at given position

--- data-structures/linked-list.py
+++ data-structures/linked-list.py
@@ -6,6 +6,18 @@ class SinglyLinkedListNode:
 class SinglyLinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
 
 def print_singly_linked_list(node, sep, fptr):
     while node:
@@ -39,6 +51,17 @@ def insertNodeAtHead(head, data):
     head = nnode
     return head
 
+# Insert node at given position
+def insertNodeAtPosition(head, data, position):
+    if head is None:
+        return head
+    current = head
+    nnode = SinglyLinkedListNode(data)
+    for i in range(position-1):
+        current = current.next
+    nnode.next = current.next
+    current.next = nnode
+    return head
 
 if __name__ == '__main__':
     fptr = open('./output', 'w')
@@ -48,10 +71,14 @@ if __name__ == '__main__':
 
     for _ in range(llist_count):
         llist_item = int(input())
-        llist_head = insertNodeAtHead(llist.head, llist_item)
-        llist.head = llist_head
+        llist.insert_node(llist_item)
 
-    print_singly_linked_list(llist.head, '\n', fptr)
+    data = int(input())
+    position = int(input())
+
+    llist_head = insertNodeAtPosition(llist.head, data, position)
+
+    print_singly_linked_list(llist_head, ' ', fptr)
     fptr.write('\n')
 
     fptr.close()
